4f78733ec04a276cbfe5ef73ba272d4376ec56b5
roberto <roberto.ernesto.castro@gmail.com>
2018-10-03 13:52:26 -0600

    Push inicial para ep2

---
+++ js/iniciar_sesion.js
@@ -0,0 +1,19 @@
+var moderadores = ["paty", "rober", "cris", "vero"]
+var administrador = "mari";
+var passModeradores = ["tpi115", "doberto115", "tpi115", "tpi115"];
+var passAdmin = "tpi115_marisol";
+
+function iniciarSesion(){
+	var usuario = document.getElementById('txtUser').value;
+	var pass = document.getElementById('txtPass').value;
+	console.log(usuario, pass);
+	for(i = 0; i < moderadores.length; i++){
+		if (moderadores[i] == usuario && pass == passModeradores[i]){
+			window.location.href = "www.google.com";
+		}
+		else{
+			if (usuario == administrador && pass == passAdmin)
+				window.location.href = "administracion/moderadores.html" ;
+		}
+	}
+}
