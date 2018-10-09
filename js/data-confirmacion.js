ea1813f6b4974583ce532c5ff96b29bde207b85c
Veronica C <svcl_svcl@hotmail.com>
2018-10-09 13:12:43 -0600

    Meta informacion, data-...

---
+++ js/data-confirmacion.js
@@ -0,0 +1,11 @@
+function confirmacion(e){
+	alert(e.dataset.confirma+"");
+}
+
+function aprobada(e){
+	alert(e.dataset.mensajea+", bien hecho.");
+}
+
+function reportada(e){
+	alert(e.dataset.mensajer+" al admin.");
+}
